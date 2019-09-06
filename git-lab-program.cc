ae972b430fc6c5a1a0c56d78257ab2557e15f07d
John Wallace <jw388918@ohio.edu>
2019-09-06 17:28:38 -0400

    This is my Hello Git Program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+*        File: git-lab-program.cc
+*      Author: John Wallace
+ *        Date: September 6th, 2019
+ * Description: Lab 2
+ */
+
+ #include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
